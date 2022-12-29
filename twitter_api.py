00e35c9af1a0b16b6850be6978d2e8d04662f0b2
Abdu Sallouh <31540503+abduABS@users.noreply.github.com>
2022-12-29 14:18:37 +0400

    Added the files

---
+++ twitter_api.py
@@ -0,0 +1,18 @@
+import tweepy
+import configparser
+
+#read configs
+config = configparser.ConfigParser(interpolation=None)
+config.read('config.ini')
+
+api_key = config['twitter']['api_key']
+api_key_secret = config['twitter']['api_key_secret']
+bearer_token = config['twitter']['bearer_token']
+
+access_token = config['twitter']['access_token']
+access_token_secret = config['twitter']['access_token_secret']
+
+client_ID = config['twitter']['client_ID']
+client_Id_secret = config['twitter']['client_Id_secret']
+
+print(api_key)
