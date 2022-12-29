84e8397cec4642e05286c8cd641bbbba5eeb0b44
Abdu Sallouh <31540503+abduABS@users.noreply.github.com>
2022-12-29 14:27:57 +0400

    added more lines of code

--- twitter_api.py
+++ twitter_api.py
@@ -16,3 +16,12 @@ client_ID = config['twitter']['client_ID']
 client_Id_secret = config['twitter']['client_Id_secret']
 
 print(api_key)
+
+#authentication
+auth = tweepy.OAuthHandler(api_key,api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+
+print(public_tweets)
