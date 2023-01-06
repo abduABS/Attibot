959c7807c1ffa90cbacc23ea658afa8eaf0c70b8
Abdu Sallouh <31540503+abduABS@users.noreply.github.com>
2023-01-06 15:04:23 +0400

    enhanced testing for the NLP algorithm

--- NPLalgorithm.py
+++ NPLalgorithm.py
@@ -1,26 +1,29 @@
+import re
 import pandas as pd
-from nltk.tokenize import TweetTokenizer
-import numpy as np
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
 
 sheet = pd.read_csv('tweets.csv')
 y = sheet.iloc[:, 0]
 X = sheet
-# print(y)
-# print(type(y))
 
-from sklearn.feature_extraction.text import CountVectorizer
-vectorizer = CountVectorizer()
+vectorizer = CountVectorizer(stop_words='english')
 
 tweets = [row[1] for index, row in sheet.iterrows()]
 
+
 X = vectorizer.fit_transform(tweets)
 X = pd.DataFrame(X.toarray(), columns=vectorizer.get_feature_names_out())
-print(type(X))
-log_reg = LogisticRegression(max_iter=1000).fit(X, y)
-print('Accuracy of logistic regression: ', log_reg.score(X, y))
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-print(log_reg.predict(X))
+# Build a logistic regression model and print out the accuracy
+log_reg = LogisticRegression(penalty='l2', C=0.59, max_iter=1000).fit(X_train, y_train)
+print('Accuracy on train set: ', log_reg.score(X_train, y_train))
+print('Accuracy on test set: ', log_reg.score(X_test, y_test))
 
+# No test set here
+# log_reg = LogisticRegression(max_iter=1000).fit(X, y)
+# print('Accuracy of logistic regression: ', log_reg.score(X, y))
+# print(log_reg.predict(X))
