0e25e82d15ac80f6750e86e5be6820760927a8e7
Abdu Sallouh <31540503+abduABS@users.noreply.github.com>
2023-01-15 19:46:05 +0400

    figured out searching??

--- twitter_api.py
+++ twitter_api.py
@@ -22,36 +22,42 @@ auth = tweepy.OAuthHandler(api_key,api_key_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-api = tweepy.API(auth)
+#tweepy client
+client = tweepy.Client(bearer_token=bearer_token,return_type=list)
+tweets = client.search_recent_tweets('potato')
+for tweet in tweets:
+    print(tweet)
 
-# Account to track
-track_account = "TheAttiBot"
 
-# Get the most recent tweet that mentions the track account
-mentions = api.search_tweets(q=f"@{track_account}", count=1)
 
-# Extract the text of the tweet
-most_recent_mention = mentions[0].text
-
-# Extract the user screen name
-screen_name = mentions[0].user.screen_name
-
-# Extract the tweet id
-tweet_id = mentions[0].id
-
-# Print the most recent mention
-print(most_recent_mention)
-
-# Your reply message
-reply_message = "I have noticed you. This is an automated message"
-
-# Reply to the tweet
-api.update_status(
-    f"@{screen_name} {reply_message}",
-    in_reply_to_status_id=tweet_id
-)
-
-print("Reply sent!")
+# # Account to track
+# track_account = "TheAttiBot"
+#
+# # Get the most recent tweet that mentions the track account
+# mentions = api.search_tweets(q=f"@{track_account}", count=1)
+#
+# # Extract the text of the tweet
+# most_recent_mention = mentions[0].text
+#
+# # Extract the user screen name
+# screen_name = mentions[0].user.screen_name
+#
+# # Extract the tweet id
+# tweet_id = mentions[0].id
+#
+# # Print the most recent mention
+# print(most_recent_mention)
+#
+# # Your reply message
+# reply_message = "I have noticed you. This is an automated message"
+#
+# # Reply to the tweet
+# api.update_status(
+#     f"@{screen_name} {reply_message}",
+#     in_reply_to_status_id=tweet_id
+# )
+#
+# print("Reply sent!")
 
 
 
