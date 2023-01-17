01a0a6d46c7b90a2f279503fd9a35e3793412847
Abdu Sallouh <31540503+abduABS@users.noreply.github.com>
2023-01-17 23:35:51 +0400

    figured out keyword recognition as well as prepared searching for the NLP algorithm

--- twitter_api.py
+++ twitter_api.py
@@ -1,5 +1,6 @@
 import tweepy
 import configparser
+import pandas as pd
 
 #read configs
 config = configparser.ConfigParser(interpolation=None)
@@ -15,41 +16,50 @@ access_token_secret = config['attibot']['secret']
 client_ID = config['twitter']['client_ID']
 client_Id_secret = config['twitter']['client_Id_secret']
 
-print(api_key)
 
 #authentication
 auth = tweepy.OAuthHandler(api_key,api_key_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+
+# Account to track
+track_account = "TheAttiBot"
+
+# Get the most recent tweet that mentions the track account
+mentions = api.search_tweets(q=f"@{track_account}", count=1)
+
+# Extract the text of the tweet
+most_recent_mention = mentions[0].text
+keyword = most_recent_mention.split()[-1].lower()
+
+
+# Extract the user screen name
+screen_name = mentions[0].user.screen_name
+
+# Extract the tweet id
+tweet_id = mentions[0].id
+
+# Print the most recent mention
+print(most_recent_mention)
+
 #tweepy client
-client = tweepy.Client(bearer_token=bearer_token,return_type=list)
-tweets = client.search_recent_tweets('potato')
-for tweet in tweets:
-    print(tweet)
+client = tweepy.Client(bearer_token=bearer_token)
+response = client.search_recent_tweets(keyword)
+
+tweets = response[0]
+
+# Convert the list of dictionaries to a pandas DataFrame
+df = pd.DataFrame(tweets)
+df = df.drop(columns=["edit_history_tweet_ids","id"])
+print(df)
+
+# for tweet in response[0]:
+#     print(tweet)
 
 
-
-# # Account to track
-# track_account = "TheAttiBot"
-#
-# # Get the most recent tweet that mentions the track account
-# mentions = api.search_tweets(q=f"@{track_account}", count=1)
-#
-# # Extract the text of the tweet
-# most_recent_mention = mentions[0].text
-#
-# # Extract the user screen name
-# screen_name = mentions[0].user.screen_name
-#
-# # Extract the tweet id
-# tweet_id = mentions[0].id
-#
-# # Print the most recent mention
-# print(most_recent_mention)
-#
-# # Your reply message
-# reply_message = "I have noticed you. This is an automated message"
+# Your reply message
+# reply_message = f'I have looked up {most_recent_mention} on twitter. Nothing yet to report.'
 #
 # # Reply to the tweet
 # api.update_status(
