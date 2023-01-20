0d7b6d9e7a327e78e25843d7082e0c617c9aece9
Abdu Sallouh <31540503+abduABS@users.noreply.github.com>
2023-01-20 19:21:51 +0400

    added timed requests

--- twitter_api.py
+++ twitter_api.py
@@ -6,6 +6,7 @@ Written by: Abdu Sallouh Twitter: @abdusallouh GitHub: @abduABS
 """
 import tweepy
 import configparser
+import time
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from os import environ
 
@@ -53,71 +54,77 @@ api = tweepy.API(auth)
 track_account = "TheAttiBot"
 
 # Get the most recent tweet that mentions the track account
-mentions = api.search_tweets(q=f"@{track_account}", count=1)
+saved_id = ''
+while True:
+    mentions = api.search_tweets(q=f"@{track_account}", count=1)
 
-# Extract the text of the tweet
-most_recent_mention = mentions[0].text
-keyword = most_recent_mention.split()[-1].lower()
+    # Extract the text of the tweet
+    most_recent_mention = mentions[0].text
+    keyword = most_recent_mention.split()[-1].lower()
 
-# Extract the user screen name
-screen_name = mentions[0].user.screen_name
+    # Extract the user screen name
+    screen_name = mentions[0].user.screen_name
 
-# Extract the tweet id
-tweet_id = mentions[0].id
+    # Extract the tweet id
+    tweet_id = mentions[0].id
 
-# Print the most recent mention
-print(most_recent_mention)
+    if tweet_id != saved_id:
+        saved_id = tweet_id
+        # Print the most recent mention
+        print(most_recent_mention)
 
-# Tweepy client
-client = tweepy.Client(bearer_token=bearer_token)
-response = client.search_recent_tweets(keyword, max_results=20)
+        # Tweepy client
+        client = tweepy.Client(bearer_token=bearer_token)
+        response = client.search_recent_tweets(keyword, max_results=20)
 
-# Get the tweets
-tweets = response[0]
+        # Get the tweets
+        tweets = response[0]
 
-# Initialize the VADER sentiment analyzer
-analyzer = SentimentIntensityAnalyzer()
+        # Initialize the VADER sentiment analyzer
+        analyzer = SentimentIntensityAnalyzer()
 
-# Initialize the sentiment score
-sentiment = 0
+        # Initialize the sentiment score
+        sentiment = 0
 
-# The tweets to be analyzed
-for i in range(len(tweets)):
-    # Get the text of the tweet
-    text = tweets[i]['text']
+        # The tweets to be analyzed
+        for i in range(len(tweets)):
+            # Get the text of the tweet
+            text = tweets[i]['text']
 
-    # Get the sentiment scores
-    sentiment_scores = analyzer.polarity_scores(text)
+            # Get the sentiment scores
+            sentiment_scores = analyzer.polarity_scores(text)
 
-    # Print the sentiment score
-    # print(sentiment_scores)
+            # Print the sentiment score
+            # print(sentiment_scores)
 
-    # Get the compound score
-    compound_score = sentiment_scores['compound']
+            # Get the compound score
+            compound_score = sentiment_scores['compound']
 
-    # Print the compound score
-    # print(compound_score)
+            # Print the compound score
+            # print(compound_score)
 
-    # Sum up the compound scores
-    sentiment += compound_score
+            # Sum up the compound scores
+            sentiment += compound_score
 
-# Overall aggregated sentiment
-sentiment = sentiment / len(tweets)
-rating = 'Positive' if sentiment > 0 else 'Negative'
+        # Overall aggregated sentiment
+        sentiment = sentiment / len(tweets)
+        rating = 'Positive' if sentiment > 0 else 'Negative'
 
-# The reply message
-reply_message = f'I have looked up "{keyword}" on twitter. The average sentiment is {rating} with a score of {sentiment:.2f}.'
-print(reply_message)
+        # The reply message
+        reply_message = f'I have looked up "{keyword}" on twitter. The average sentiment is {rating} with a score of {sentiment:.2f}.'
+        print(reply_message)
 
 
-# Reply to the tweet
-api.update_status(
-    f"@{screen_name} {reply_message}",
-    in_reply_to_status_id=tweet_id
-)
+        # Reply to the tweet
+        api.update_status(
+            f"@{screen_name} {reply_message}",
+            in_reply_to_status_id=tweet_id
+        )
 
-# Log for debugging
-print("Reply sent!")
+        # Log for debugging
+        print("Reply sent!")
+
+        time.sleep(120)
 
 
 
